73f77abecaed04c3fd296855a0eeec054bb91313
大石头 <nnhy@vip.qq.com>
2019-02-17 21:25:29 +0800

    建立轻量级消息队列，满足日常工作80%需求

---
+++ NewLife.MQ/MQSession.cs
@@ -0,0 +1,11 @@
+﻿using NewLife.Remoting;
+
+namespace NewLife.MessageQueue
+{
+    /// <summary>MQ会话</summary>
+    [Api(null)]
+    public class MQSession : IApi
+    {
+        public IApiSession Session { get; set; }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using NewLife.Log;
+using NewLife.MessageQueue;
+using NewLife.Serialization;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            XTrace.UseConsole();
+
+            Test1();
+
+            Console.WriteLine("OK!");
+            Console.ReadKey();
+        }
+
+        static void Test1()
+        {
+        }
+
+        static void Test2()
+        {
+        }
+
+        static void Test3()
+        {
+            
+        }
+    }
+}
